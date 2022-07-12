eb14c5b165e185228243642bf197d3631c732fee
epk <erik@erikpkraft.com>
2022-07-11 20:47:55 -0400

    Calendar update
    Fixed the calendar, pushing this before adding photos to site.

--- calendar.php
+++ calendar.php
@@ -14,7 +14,8 @@
 
 <div class="events_main">
   <h1 class="calendar">Upcoming Events</h1>
-  <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=bGl0dGxldG9uY3VsdHVyYWxjb3VuY2lsLm9yZ184ZGRqbGhubjhzbW92aTU1czJwNHMybzZpZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23D81B60&showCalendars=0&showTabs=0&showDate=1&showTitle=1" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no" padding="15px"></iframe>
+<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=aW5mb0BsaXR0bGV0b25jdWx0dXJhbGNvdW5jaWwub3Jn&src=bGl0dGxldG9uY3VsdHVyYWxjb3VuY2lsLm9yZ184ZGRqbGhubjhzbW92aTU1czJwNHMybzZpZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%23D81B60" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no" padding="15px"></iframe>
+  <!-- <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=bGl0dGxldG9uY3VsdHVyYWxjb3VuY2lsLm9yZ184ZGRqbGhubjhzbW92aTU1czJwNHMybzZpZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23D81B60&showCalendars=0&showTabs=0&showDate=1&showTitle=1" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no" padding="15px"></iframe> -->
 </div>
         <div class="footer">
         <?php include("includes/footer.php"); ?>
