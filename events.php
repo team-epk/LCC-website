ba43da3450ffc2baf97913a8e2fc0054ad9b3125
epk <erik@erikpkraft.com>
2022-07-11 21:57:08 -0400

    Event updates July 2022
    Added rock painting and A Musical Celebration photos.

--- events.php
+++ events.php
@@ -26,14 +26,30 @@
         </div>
 <div class="event_photos">
   <h1 class="grantees">Recent Grantee Events</h1>
-  <div id="senseOfPlace2021">
+  <div id="senseOfPlace2021" class="eventPhotos">
     <h3 class="grantees">A Sense of Place Opening Reception - September 26, 2021</h3>
   <a rel="senseOfPlace2021" href="img/events/senseOfPlace2021/IMG_6483.JPG" class="swipebox" title="Sense of Place Reception, September 26, 2021"><img src="img/events/senseOfPlace2021/thumbs/IMG_6483_thumbnail.jpg" alt="a painting hung as part of the Sense of Place opening reception."></a>
   <a rel="senseOfPlace2021" href="img/events/senseOfPlace2021/IMG_6484.JPG" class="swipebox" title="Sense of Place Reception, September 26, 2021"><img src="img/events/senseOfPlace2021/thumbs/IMG_6484_thumbnail.jpg" alt="a painting hung as part of the Sense of Place opening reception."></a>
   <a rel="senseOfPlace2021" href="img/events/senseOfPlace2021/IMG_6483.JPG" class="swipebox" title="Sense of Place Reception, September 26, 2021"><img src="img/events/senseOfPlace2021/thumbs/IMG_6483_thumbnail.jpg" alt="a photo hung as part of the Sense of Place opening reception."></a>
   <a rel="senseOfPlace2021" href="img/events/senseOfPlace2021/IMG_6485.JPG" class="swipebox" title="Sense of Place Reception, September 26, 2021"><img src="img/events/senseOfPlace2021/thumbs/IMG_6485_thumbnail.jpg" alt="Patrons enjoy the art at the Sense of Place opening reception."></a>
   <a rel="senseOfPlace2021" href="img/events/senseOfPlace2021/IMG_6487.JPG" class="swipebox" title="Sense of Place Reception, September 26, 2021"><img src="img/events/senseOfPlace2021/thumbs/IMG_6487_thumbnail.jpg" alt="Signage regarding the purpose of the Sense of Place show."></a>
-  <a rel="senseOfPlace2021" href="img/events/senseOfPlace2021/IMG_6490.JPG" class="swipebox" title="Sense of Place Reception, September 26, 2021"><img src="img/events/senseOfPlace2021/thumbs/IMG_6490_thumbnail.jpg" alt="an antique wooden desk at the Sense of Place opening reception."></a></div>
+  <a rel="senseOfPlace2021" href="img/events/senseOfPlace2021/IMG_6490.JPG" class="swipebox" title="Sense of Place Reception, September 26, 2021"><img src="img/events/senseOfPlace2021/thumbs/IMG_6490_thumbnail.jpg" alt="an antique wooden desk at the Sense of Place opening reception."></a>
+</div>
+  <div id="rockPainting2022" class="eventPhotos">
+    <h3 class="grantees">Painting rocks in preparation for the labyrinth opening - June 23, 2022.</h3>
+    <a rel="rockPainting2022" href="img/events/rockPainting2022/IMG_0715.jpg" class="swipebox" title="Painting rocks in preparation for labyrinth opening."><img src="img/events/rockPainting2022/thumbs/IMG_0715_thumbnail.jpg" alt="A child paints a rock at a table outdoors."></a>
+    <a rel="rockPainting2022" href="img/events/rockPainting2022/IMG_0716.jpg" class="swipebox" title="Painting rocks in preparation for labyrinth opening."><img src="img/events/rockPainting2022/thumbs/IMG_0716_thumbnail.jpg" alt="An LCC member does something with a piece of paper."></a>
+    <a rel="rockPainting2022" href="img/events/rockPainting2022/IMG_0717.jpg" class="swipebox" title="Painting rocks in preparation for labyrinth opening."><img src="img/events/rockPainting2022/thumbs/IMG_0717_thumbnail.jpg" alt="A painter displays their handiwork."></a>
+    </div>
+    <div id="tagoreDance2022" class="eventPhotos">
+    <h3 class="grantees">A Musical Celebration at Sargent Memorial Library - June 25th, 2022.</h3>
+  <a rel="tagoreDance2022" href="img/events/tagoreDance2022/IMG_0726.jpg" class="swipebox" title="A Musical Celebration at Sargent Memorial Library, June 25th, 2022."><img src="img/events/tagoreDance2022/thumbs/IMG_0726_thumbnail.jpg" alt="Introductory slide."></a>
+  <a rel="tagoreDance2022" href="img/events/tagoreDance2022/IMG_0728.jpg" class="swipebox" title="A Musical Celebration at Sargent Memorial Library, June 25th, 2022."><img src="img/events/tagoreDance2022/thumbs/IMG_0728_thumbnail.jpg" alt="The participants."></a>
+  <a rel="tagoreDance2022" href="img/events/tagoreDance2022/IMG_0729.jpg" class="swipebox" title="A Musical Celebration at Sargent Memorial Library, June 25th, 2022."><img src="img/events/tagoreDance2022/thumbs/IMG_0729_thumbnail.jpg" alt="Percussionist at work."></a>
+  <a rel="tagoreDance2022" href="img/events/tagoreDance2022/IMG_0730.jpg" class="swipebox" title="A Musical Celebration at Sargent Memorial Library, June 25th, 2022."><img src="img/events/tagoreDance2022/thumbs/IMG_0730_thumbnail.jpg" alt="Trio of musicians."></a>
+  <a rel="tagoreDance2022" href="img/events/tagoreDance2022/IMG_0731.jpg" class="swipebox" title="A Musical Celebration at Sargent Memorial Library, June 25th, 2022."><img src="img/events/tagoreDance2022/thumbs/IMG_0731_thumbnail.jpg" alt="Close up of two of the musicians."></a>
+  <a rel="tagoreDance2022" href="img/events/tagoreDance2022/IMG_0732.jpg" class="swipebox" title="A Musical Celebration at Sargent Memorial Library, June 25th, 2022."><img src="img/events/tagoreDance2022/thumbs/IMG_0732_thumbnail.jpg" alt="Close up of one of the trio of musicians."></a>
+  </div>
   <h2 class="grantees">Be sure to check the <a href="calendar.php">LCC calendar</a> for upcoming grantee events!</h2>
 </div>
         <div class="footer">
